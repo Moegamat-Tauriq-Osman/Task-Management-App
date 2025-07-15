c4e62707e8e8bcbad53861b66aab658411afaf7a
moega <moegamattauriqosman@gmail.com>
2025-07-15 14:49:47 +0200

    Migration for role added  to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['Admin', 'Team Member', 'Guest'])->default('Guest');
             $table->rememberToken();
             $table->timestamps();
         });
