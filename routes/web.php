f6234592300f0648bee8cf45289ffc1fe0651280
moega <moegamattauriqosman@gmail.com>
2025-07-17 20:21:57 +0200

    Fixed layout file name and added and updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,23 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TITtaskController;
+use App\Http\Controllers\TITcategoryController;
+use App\Http\Controllers\TITdashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [TITdashboardController::class, 'index'])->name('dashboard');
+    Route::resource('tasks', TITtaskController::class);
+    Route::resource('categories', TITcategoryController::class);
 
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
