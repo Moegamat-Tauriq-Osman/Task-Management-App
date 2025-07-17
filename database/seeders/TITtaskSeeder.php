751c2f88f839bd1cb11c1a28a22a6a8c179d54a4
taliasmuts28 <taliasmuts28@gmail.com>
2025-07-17 19:35:19 +0200

    seeder file created

---
+++ database/seeders/TITtaskSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TITtaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
